74bf11cc122d5d43b09b52ec7407906363c9c019
CISCO <cisco@iicsadmin.com>
2020-02-24 12:32:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+let person = {name:"Juan Dela Cruz", gender: "Male", age: 25};
+let person2 = {name: "Cathy Garcia", gender: "Female", age: 20}
+console.log(`${person.name} is ${person.gender}`);
+console.log(`${person2.name} is a ${person2.gender} person whose age is ${person2.age}`)
